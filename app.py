faf33442888541498453f248c7ff04efec7087c4
Trunki <hahn.silvan@gmx.ch>
2024-03-25 11:16:16 +0100

    inital commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def api_call():  # put application's code here
-    return '<div>Hello Worlddde!<div>'
+    return '<div>Hettllo Worlddde!<div>'
 
 
 if __name__ == '__main__':
