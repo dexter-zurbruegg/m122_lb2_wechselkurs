31c2ff4b7ba0640057d96781fbaeded13c18a88e
Trunki <hahn.silvan@gmx.ch>
2024-05-06 09:39:50 +0200

    API Keys (Kleine hilfe)

--- .idea/M122_Website_applikation/config.py
+++ .idea/M122_Website_applikation/config.py
@@ -1,5 +1,5 @@
 # API-Schlüssel für die Exchange Rate API
-EXCHANGE_RATE_API_KEY = "DEIN_API_KEY"
+EXCHANGE_RATE_API_KEY = "07b43fd62ae9d052c6ab74c6"
 
 # API-Schlüssel für die Free Currency API
-FREE_CURRENCY_API_KEY = "DEIN_API_KEY"
+FREE_CURRENCY_API_KEY = "fca_live_nhyMBRl1RzbqxspWdOP9s42ZsQ6Srr8SQLyOLhgk"
