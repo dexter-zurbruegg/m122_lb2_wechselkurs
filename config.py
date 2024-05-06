2ee7cd88f82fde79bbe48daf2797734f73514f19
rei9050 <dexter.zurbruegg@edu.tbz.ch>
2024-05-06 09:10:43 +0200

    unused imports

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-import os
-import export
-
 API_KEY = 'ae2a87ed71df46cede889e85'
 API_URL = f'https://v6.exchangerate-api.com/v6/{API_KEY}/latest/CHF'
 ALT_API_KEY = 'fca_live_7LMTft5sGhrRQjdDQGGEtT8XTedjLqkyOJHztFe1'
